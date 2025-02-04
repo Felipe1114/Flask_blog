30ad59356a4464ae9e9f960a4a036984cdb4d949
Felipe1114 <63155769+Felipe1114@users.noreply.github.com>
2025-02-04 14:25:38 +0100

    CRUD class in app.py eingebaut

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from flask import render_template
+from programm_modules import CRUD
 
+blog_post_file_path = 'programm_database/blog_posts.json'
 app = Flask(__name__)
+blog_data = CRUD.Crud(blog_post_file_path)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+
+@app.route('/p')
+def index():
+    blog_posts = blog_data.get_posts()
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
